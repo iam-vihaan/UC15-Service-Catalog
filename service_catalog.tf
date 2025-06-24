23943afd10577c7a96a908a1f409ef103d0c6159
iam-vihaan <asathyavihaan@gmail.com>
2025-06-24 12:15:50 +0530

    Create service_catalog.tf

---
+++ service_catalog.tf
@@ -0,0 +1,32 @@
+
+resource "aws_servicecatalog_portfolio" "web_app_portfolio" {
+  name          = "WebAppPortfolio"
+  description   = "Portfolio for launching web applications"
+  provider_name = "My-Comp"
+}
+
+resource "aws_servicecatalog_product" "web_app_product" {
+  name        = "WebAppProduct"
+  owner       = "My-Comp"
+  description = "Launch a web application"
+  distributor = "My-Comp"
+  type        = "CLOUD_FORMATION_TEMPLATE"
+
+  provisioning_artifact_parameters {
+    name         = "v1"
+    description  = "Initial version"
+    template_url = "https://kasi-hcl-bucket-uc8.s3.us-east-1.amazonaws.com/SC-Templeate/CFT.txt"
+    type         = "CLOUD_FORMATION_TEMPLATE"
+  }
+}
+
+resource "aws_servicecatalog_portfolio_product_association" "association" {
+  portfolio_id = aws_servicecatalog_portfolio.web_app_portfolio.id
+  product_id   = aws_servicecatalog_product.web_app_product.id
+}
+
+resource "aws_servicecatalog_launch_constraint" "launch_role" {
+  portfolio_id = aws_servicecatalog_portfolio.web_app_portfolio.id
+  product_id   = aws_servicecatalog_product.web_app_product.id
+  role_arn     = "arn:aws:iam::123456789012:role/ServiceCatalogLaunchRole"
+}
