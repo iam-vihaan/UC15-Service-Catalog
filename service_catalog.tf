4a80b4f2cc54dffac98426400f88dbb9251066a8
iam-vihaan <asathyavihaan@gmail.com>
2025-06-24 15:29:29 +0530

    Update service_catalog.tf

--- service_catalog.tf
+++ service_catalog.tf
@@ -1,37 +1,41 @@
 
-resource "aws_servicecatalog_portfolio" "web_app_portfolio" {
-  name          = "WebAppPortfolio"
-  description   = "Portfolio for launching web applications"
-  provider_name = "My-Comp"
+# Create a Service Catalog Portfolio
+resource "aws_servicecatalog_portfolio" "example" {
+  name          = "ExamplePortfolio"
+  description   = "Portfolio for example products"
+  provider_name = "ExampleProvider"
 }
 
-resource "aws_servicecatalog_product" "web_app_product" {
-  name        = "WebAppProduct"
-  owner       = "My-Comp"
-  description = "Launch a web application"
-  distributor = "My-Comp"
-  type        = "CLOUD_FORMATION_TEMPLATE"
+# Create a Service Catalog Product
+resource "aws_servicecatalog_product" "example" {
+  name          = "ExampleProduct"
+  owner         = "ExampleOwner"
+  description   = "An example product"
+  distributor   = "ExampleDistributor"
+  support_description = "Contact support@example.com"
+  type          = "CLOUD_FORMATION_TEMPLATE"
 
   provisioning_artifact_parameters {
-    name         = "v1"
-    description  = "Initial version"
-    template_url = "https://kasi-hcl-bucket-uc8.s3.us-east-1.amazonaws.com/SC-Templeate/CFT.txt"
-    type         = "CLOUD_FORMATION_TEMPLATE"
+    name           = "v1"
+    description    = "Initial version"
+    template_url   = "https://kasi-hcl-bucket-uc8.s3.us-east-1.amazonaws.com/SC-Templeate/CFT.txt"
+    type           = "CLOUD_FORMATION_TEMPLATE"
   }
 }
 
+# Associate an IAM Role with the Portfolio
 resource "aws_servicecatalog_principal_portfolio_association" "association" {
-  portfolio_id = aws_servicecatalog_portfolio.example.id
+  portfolio_id  = aws_servicecatalog_portfolio.example.id
   principal_arn = "arn:aws:iam::784733659029:role/SC-role"
 }
 
-
+# Add a Launch Constraint to the Product
 resource "aws_servicecatalog_constraint" "launch_role" {
   portfolio_id = aws_servicecatalog_portfolio.example.id
   product_id   = aws_servicecatalog_product.example.id
   type         = "LAUNCH"
-  parameters   = jsonencode({
+
+  parameters = jsonencode({
     RoleArn = "arn:aws:iam::784733659029:role/SC-role"
   })
 }
-
